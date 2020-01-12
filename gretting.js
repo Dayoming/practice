0fda2c369bd748d78c2bb4550b1091b4b327cfc7
Dayoming <jdayoung1204@naver.com>
2020-01-12 22:15:33 +0900

    #3-4 주석 추가 및 오류 수정

--- gretting.js
+++ gretting.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".js-form"),
-input = document.querySelector("input"),
+input = form.querySelector("input"),
 greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
@@ -10,7 +10,9 @@ function saveName(text) {
 }
 
 function handleSubmit(event) {
+    // form을 제출하는 이벤트의 기본 동작을 막음(새로고침 안됨)
     event.preventDefault();
+
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
@@ -30,7 +32,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
-
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
@@ -40,4 +42,5 @@ function init() {
     loadName();
 }
 
+// LocalStorege는 URLS를 기본으로 동작
 init();
