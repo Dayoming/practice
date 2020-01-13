0ae5dbe01cff9ce28bbd1b315ba5fec7cea06c60
Dayoming <jdayoung1204@naver.com>
2020-01-14 04:54:54 +0900

    #3-8 백그라운드 이미지 추가 @keyframes -> 모양의 변화를 정하는 속성

---
+++ bg.js
@@ -0,0 +1,31 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 4;
+
+// function handleImgLoad() {
+//     console.log("finished loading");
+// }
+
+function paintImage(imgNumber) {
+    // image 안에 새로운 object를 만듬
+    const image = new Image();
+    image.src = `/images/${imgNumber}.jpg`;
+    // image element class 안에 bgImage 추가
+    image.classList.add("bgImage");
+    body.appendChild(image);
+    // API를 사용할 때
+    // image.addEventListener("loadend", handleImgLoad);
+}
+
+function getRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER + 1);
+    return number;
+}
+
+function init() {
+    const randomNumber = getRandom();
+    console.log(randomNumber);
+    paintImage(randomNumber);
+}
+
+init();
