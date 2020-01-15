227a8342e97e4d87319ace4fac0d0ebabbf00a06
Dayoming <jdayoung1204@naver.com>
2020-01-15 22:51:57 +0900

    #3-10 날씨 API를 이용해 현재 위치, 날씨 표기하기 fetch -> 해당 사이트에 request를 보냄 then -> fetch가 실행될 때까지 기다렸다가 해당 함수를 실행함
    받아온 response를 json으로 반환받고 이용하여 적용함.

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const API_KEY = '77dbe507f648f054bf1af23f91fbbf7c';
+const weather = document.querySelector('.js-weather');
 const COORDS = 'coords';
+
+function getWeather(lat, lon) {
+    // fetch는 해당 사이트에 request를 보냄
+    // then은 fetch가 실행될때까지 기다렸다가 해당 함수를 실행함
+
+    // api 서버에 요청을 보내고, 해당 요청이 끝나면 받아온 response 정보를 json으로 바꿈
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appId=${API_KEY}&units=metric`)
+    .then(function(response) {
+       return response.json();
+    })
+    .then(function(json) {
+        const temp = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temp} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -15,6 +33,7 @@ function handleGeoSuccess(position) {
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -26,11 +45,14 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadCords = localStorage.getItem(COORDS);
-    if(loadCords === null) {
+    const loadCoords = localStorage.getItem(COORDS);
+    // local storage에 현재 위치가 존재하지 않으면
+    if(loadCoords === null) {
         askForCoords();
+    // 존재하면
     } else {
-        // getWeather
+        const parsedCoords = JSON.parse(loadCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
