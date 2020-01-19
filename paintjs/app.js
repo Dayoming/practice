63571b86324f32f4a667cf7f405e8412b47e90a5
Dayoming <jdayoung1204@naver.com>
2020-01-19 23:28:53 +0900

    캔버스에 첫번째 지점/마지막 지점 좌표를 이용해 선 그리기 (다시 한번 살펴보기!)

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,4 +1,12 @@
+// canvas에는 width와 height를 지정해줘야함
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,11 +14,23 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     // clientXY -> 전체 화면에서의 좌표
     // offsetXY -> 해당 div 안에서의 좌표
     const x = event.offsetX;
     const y = event.offsetY;
+
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y); 
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
@@ -18,16 +38,16 @@ function onMouseDown(event) {
 }
 
 function onMouseUp(event) {
-    
+    stopPainting();
 }
 
 if(canvas) {
     // 캔버스 위에서 마우스가 움직일 때
     canvas.addEventListener("mousemove", onMouseMove);
     // 캔버스 위를 클릭했을 때
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     // 캔버스 위를 클릭하고 뗐을 때
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseup", stopPainting);
     // 마우스가 캔버스를 떠나면
     canvas.addEventListener("mouseleave", stopPainting);
 }
